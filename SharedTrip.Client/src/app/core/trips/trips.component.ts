922fee6d2d7797710f0475af2e62b9211a090aea
Yanislav Ivanov <frexcho998@gmail.com>
2022-03-24 23:51:35 +0200

    Introduced sorting trips

--- SharedTrip.Client/src/app/core/trips/trips.component.ts
+++ SharedTrip.Client/src/app/core/trips/trips.component.ts
@@ -1,4 +1,5 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { EmptyError } from 'rxjs';
 import { ITrip } from 'src/interfaces/ITrip';
 import { TripsService } from 'src/services/trips.service';
 
@@ -11,10 +12,46 @@ export class TripsComponent {
   public tripsRecords!: Array<ITrip>;
   public recordsPages!: Array<number>;
   private data!: Array<ITrip>
-  private recordsPerPage:number = 5;
+  private recordsPerPage: number = 5;
+  private page = {
+    start: 0,
+    end: this.recordsPerPage
+  }
+  private currentSort = {
+    field: "",
+    order: ""
+  }
 
   constructor(private trips: TripsService) {
-    trips.getAllTrips().subscribe(data => {
+    this.loadData();
+  }
+
+  onClick(event: MouseEvent) {
+    let element = event.target as HTMLElement;
+    let sortTerm = this.lowerFirstLetter(this.camalize(element.textContent!)).trim();
+    debugger
+    if (this.currentSort.field == "") {
+      this.currentSort.field = sortTerm;
+      this.currentSort.order = "asc"
+      //@ts-ignore
+      this.data.sort((a, b) => (a[sortTerm] < b[sortTerm]) ? -1 : 1);
+    }
+    else if (this.currentSort.field == sortTerm && this.currentSort.order == "asc") {
+      this.currentSort.order = "desc"
+      //@ts-ignore
+      this.data.sort((a, b) => (a[sortTerm] < b[sortTerm]) ? 1 : -1);
+    }
+    else if (this.currentSort.field == sortTerm && this.currentSort.order == "desc") {
+      this.currentSort.field = '';
+      this.currentSort.order = '';
+      this.loadData()
+    }
+
+    this.tripsRecords = this.applyPaging(this.page.start, this.page.end);
+
+  }
+  loadData() {
+    this.trips.getAllTrips().subscribe(data => {
       this.data = data;
       this.tripsRecords = data.slice(0, this.recordsPerPage);
       this.recordsPages = [];
@@ -22,9 +59,20 @@ export class TripsComponent {
     });
   }
 
-  pageHandler(event:MouseEvent){
-    var currentPage = (event.currentTarget as HTMLElement).getAttribute('data-page');
-    this.tripsRecords = this.data.slice((Number(currentPage)-1)*Number(this.recordsPerPage), Number(this.recordsPerPage)*Number(currentPage));
+  onPageClick(event: MouseEvent) {
+    let currentPage = (event.currentTarget as HTMLElement).getAttribute('data-page');
+    this.tripsRecords = this.applyPaging((Number(currentPage) - 1) * Number(this.recordsPerPage), Number(this.recordsPerPage) * Number(currentPage));
+  }
+  applyPaging(start: number, end: number) {
+    this.page.start = start;
+    this.page.end = end;
+    return this.data.slice(start, end);
+  }
+  lowerFirstLetter(string: string) {
+    return string.charAt(0).toLowerCase() + string.slice(1);
+  }
+  camalize(str: string) {
+    return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
   }
   populatePages(recordsPages: Array<number>) {
     let counter = 1;
